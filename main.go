9e199be8edf31e59ef73d61fee4b84870dd84e82
mfreeman451 <mfreeman451@gmail.com>
2022-09-10 23:18:07 -0500

    CLeanup

--- main.go
+++ main.go
@@ -66,3 +66,18 @@ var longTrigger = hbot.Trigger{
 		return false
 	},
 }
+
+var SigPic = hbot.Trigger{
+	// Condition
+	func(b *hbot.Bot, m *hbot.Message) bool {
+		return m.Content == "penis"
+		// return m.From == "whyrusleeping"
+	},
+	// Action
+	func(b *hbot.Bot, m *hbot.Message) bool {
+		msg := fmt.Sprintf("%s said %s", m.From, m.Content)
+		b.Msg("#Chases", msg)
+		// b.Reply(m, "whyrusleeping said something")
+		return false
+	},
+}
